02fed1ea88c68c9e7411d13361fb3f2e3d0733e6
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-04-11 21:45:00 -0400

    Changes naming for user model & bcrypt description

--- models/User.js
+++ models/User.js
@@ -1,15 +1,15 @@
 const db = require('../db');
 
 class User {
-  static createUser(name, username, email, password) {
+  static create(name, username, email, password) {
     const queryText = 'INSERT INTO users (name, username, email, password) VALUES ($1, $2, $3, $4);';
     return db.query(queryText, [name, username, email, password]);
   }
 
-  static getUserByEmail(userEmail) {
+  static getByEmail(userEmail) {
     const queryText = 'SELECT * FROM users WHERE email = $1;';
     return db.query(queryText, [userEmail])
-      .then((response) => response.rows);
+      .then((response) => response.rows[0]);
   }
 }
 
