a41747c0fafe7ba8fe8731665b68082f0ae5b274
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-04-11 21:34:31 -0400

    Changes method and controller names, adds database

--- models/JournalEntry.js
+++ models/JournalEntry.js
@@ -1,38 +1,37 @@
 const db = require('../db');
 
 class JournalEntry {
-  static addJournalEntry(userId, title, entryBody, isPrivate) {
+  static add(userId, title, entryBody, isPrivate) {
     const queryText = 'INSERT INTO journal_entries (user_id, title, entry_body, is_private) VALUES ($1, $2, $3, $4);';
     return db.query(queryText, [userId, title, entryBody, isPrivate]);
   }
 
-  static viewJournalEntry(entryId) {
+  static view(entryId) {
     const queryText = 'SELECT * FROM journal_entries WHERE entry_id = $1;';
     return db.query(queryText, [entryId])
       .then((response) => response.rows[0]);
   }
 
-  static editJournalEntry(entryId, title, entryBody, isPrivate) {
+  static update(entryId, title, entryBody, isPrivate) {
     const queryText = 'UPDATE journal_entries SET title=$2, entry_body=$3, is_private=$4 WHERE entry_id=$1;';
     return db.query(queryText, [entryId, title, entryBody, isPrivate]);
   }
 
-  static deleteJournalEntry(entryId) {
+  static remove(entryId) {
     const queryText = 'DELETE FROM journal_entries WHERE entry_id = $1;';
-    return db.query(queryText, [entryId])
-      .then((response) => response.rows);
+    return db.query(queryText, [entryId]);
   }
 
-  static getLastCreatedEntry(userId) {
+  static getLastCreated(userId) {
     const queryText = 'SELECT * FROM journal_entries ORDER BY entry_id DESC LIMIT 1 WHERE user_id = $1;';
     return db.query(queryText, [userId])
-      .then((response) => response.rows);
+      .then((response) => response.rows[0]);
   }
 
-  static getJournalEntryById(entryId) {
+  static getById(entryId) {
     const queryText = 'SELECT * FROM journal_entries WHERE entry_id = $1;';
     return db.query(queryText, [entryId])
-      .then((response) => response.rows);
+      .then((response) => response.rows[0]);
   }
 }
 
