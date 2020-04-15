a0823d0ee070cdc02eee46907f4168cde66c5530
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2020-04-15 11:27:15 -0400

    use express json parser, require and use signup route, load .env

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const express = require('express');
+const signup = require('./routes/signup');
 
 const app = express();
 const port = process.env.PORT || 8080;
+app.use(express.json());
+
+app.use(signup);
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
