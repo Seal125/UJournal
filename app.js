695459ff046503b247cb5874715872d57ca98633
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2020-04-16 10:17:42 -0400

    use new routes and middleware

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-const signup = require('./routes/signup');
+const authenticate = require('../middleware/authenticate');
+const entriesRoute = require('./routes/entries');
+const usersRoute = require('./routes/users');
+const viewsRoute = require('./routes/views');
 
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.json());
 
-app.use(signup);
+app.use(viewsRoute);
+app.use(usersRoute);
+app.use(authenticate);
+app.use(entriesRoute);
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
