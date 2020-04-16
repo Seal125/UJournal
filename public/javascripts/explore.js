bb0a31cb8209424621be9274228f685fd2ecf51f
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-04-16 18:27:57 -0400

    Sets up links for viewing info on entry

--- public/javascripts/explore.js
+++ public/javascripts/explore.js
@@ -4,10 +4,12 @@ window.addEventListener('load', async () => {
 
   return entries.forEach((entry) => {
     document.getElementById('explore-entries').innerHTML += `
+      <a href="/entries/${entry.entry_id}">
         <div class="entry">
           <h1 class="title">${entry.title}</h1>
           <p>By ${entry.username}</p>
         </div>
+      </a>
     `;
   });
 });
