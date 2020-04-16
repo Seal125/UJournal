3e1fc8bcd3dbbab38c49cc5660b3916111144360
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2020-04-16 10:20:06 -0400

    move login controller to file matching resource

--- routes/signup.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const signupController = require('../controllers/signup');
-
-const router = express.Router();
-router.get('/signup', signupController.getSignupPage);
-router.post('/signup', signupController.signUp);
-
-module.exports = router;
