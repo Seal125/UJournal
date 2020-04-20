c9185de0c2c57b7eafdceacd39bbedaf7aae552d
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-04-19 23:57:56 -0400

    Redirects back to entries page after adding entry

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,13 +1,14 @@
 const JournalEntry = require('../models/JournalEntry');
 const tagsEntry = require('../models/Tags');
+
 const add = (req, res) => {
   const {
     userId, title, entryBody, isPrivate,
   } = req.body;
-  const { tags } = req.body
-  console.log(tags)
+  const { tags } = req.body;
+  console.log(tags);
   JournalEntry.add(userId, title, entryBody, isPrivate)
-    .then(() => res.status(200).json({ message: 'Journal entry added.' }))
+    .then(() => res.redirect('/entries'))
     .catch(() => res.status(500).json({ message: 'Journal entry could not be added.' }));
 };
 
