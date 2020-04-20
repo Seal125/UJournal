b789936fb35bdc99e42e4429478be5ebddaec5ae
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-04-19 22:44:11 -0400

    Adds logo, improves authentication

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   host: '/var/run/postgresql',
-  user: '',
-  database: '',
+  user: 'seal125',
+  database: 'journal',
   port: 5432,
 });
 
